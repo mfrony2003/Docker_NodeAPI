1ce179d2f4d72c24afd6ed7ae8997fe3e4d12300
mfrony2003 <mfrony2003@gmail.com>
2021-10-18 01:34:28 +0600

    s

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 const app = express()
 const port = 3001
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
